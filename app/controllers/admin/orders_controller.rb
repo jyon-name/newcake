368d78d328beaaea0030f645474ba241cccb30d5
nomi yohei <yo0314baseball@gmail.com>
2020-07-17 04:59:25 +0000

    注文更新

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,3 +1,16 @@
 class Admin::OrdersController < ApplicationController
-
+	def index
+		@orders =Order.all
+	end
+	def show
+ 		@order =Order.find(params[:id])
+ 		@order_details = @order.order_details
+ 		@price =@order.paying_sum - 800
+	end
+	def update
+	end
+	private
+	def order_params
+		params.require(:order).permit(:order_status)
+	end
 end
