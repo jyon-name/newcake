0fa3b650cc002cfb0f52a75a655b01331c206ecd
nomi yohei <yo0314baseball@gmail.com>
2020-07-18 14:23:10 +0000

    update

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,6 +9,15 @@ class Admin::OrdersController < ApplicationController
  		@price =@order.paying_sum - 800
 	end
 	def update
+		@order =Order.find(params[:id])
+		@order.update(order_params)
+		if @order.order_status == "recieving"
+			binding.pry
+			@order.order_details.each do |order_detail|
+				order_detail.update(cooking_status: "wait")
+			end
+		end
+		redirect_to admin_order_path(@order)
 	end
 	private
 	def order_params
